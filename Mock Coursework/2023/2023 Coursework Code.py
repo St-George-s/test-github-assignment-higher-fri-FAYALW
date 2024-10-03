f68aad83d5c49c24f07e372c69499c4d9de18b9a
Jack Walker <33574605+jackow98@users.noreply.github.com>
2024-10-03 14:01:07 +0100

    Mr Walker changes

--- Mock Coursework/2023/2023 Coursework Code.py
+++ Mock Coursework/2023/2023 Coursework Code.py
@@ -1,49 +1,59 @@
 import csv
 
+# Read data from attractions CSV into parallel arrays
 def readDataFromFile():
-    with open('Mock Coursework/Attractions.csv', 'r') as file:
+    with open('Mock Coursework/2023/Attractions.csv', 'r') as file:
         reader = csv.reader(file)
+
+        # Set up parallel arrays
         attraction = []
         category = []
         visitors = []
         daysOpen = []
         height = []
+
+        # Loop over each row and append data to parallel arrays
         for row in reader:
             attraction.append(row[0])
             category.append(row[1])
             visitors.append(int(row[2]))
             daysOpen.append(int(row[3]))
             height.append(row[4])
+
         return attraction, category, visitors, daysOpen, height
 
+# 
 def displayNamesMax(visitors, attraction):
     maximumValue = visitors[0] 
-    index = 0
+    max_index = 0
     for counter in range(1, len(visitors)): 
         if visitors[counter] > maximumValue: 
             maximumValue = visitors[counter]
-            index = counter 
+            max_index = counter 
     print("The largest value was", str(maximumValue), "for attraction", (attraction[index])) 
     
-
+# 
 def displayNamesMin(visitors, attraction):
     minimumValue = visitors[0]
-    index = 0
+    min_index = 0
     for counter in range(1, len(visitors)): 
         if visitors[counter] < minimumValue: 
             minimumValue = visitors[counter]
-            index = counter
+            min_index = counter
     print("The smallest value was", str(minimumValue), "for attraction", (attraction[index]))
 
+# 
 def writeDataToFile(daysOpen, attraction, category):
-    with open('Mock Coursework/service.csv', 'w', newline='') as file: 
+    with open('Mock Coursework/2023/service.csv', 'w', newline='') as file: 
         writer = csv.writer(file) 
-        for x in range (len(attraction)):
-            if category[x] == 'Roller Coaster':
-                days = daysOpen[x] % 90
+        for counter in range (len(attraction)):
+            if category[counter] == 'Roller Coaster':
+                days = daysOpen[counter] % 90
                 if (90 - days) <= 7:
-                    writer.writerow([attraction[x]]) 
+                    # Write array with one element to each row
+                    writer.writerow([attraction[counter]]) 
 
+# Main program
 attraction, category, visitors, daysOpen, height = readDataFromFile()
 displayNamesMax(visitors, attraction)
 displayNamesMin(visitors, attraction)
