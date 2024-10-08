c535260f6572009bc4e6ad3010d3253f872bf202
FAYALW <ALW001@stge.org.uk>
2024-10-08 09:24:32 +0000

    Part C

--- Class Test/Games Extension.py
+++ Class Test/Games Extension.py
@@ -1,14 +1,22 @@
 import csv
 
+#Defining parallel arrays
+gameTitles = []
+genres = []
+ageRatings = []
+genre_to_check = ""
+platform = []
+
 #Reading the game data from the CSV file
 def readGameDataFromCSV():
-    with open('Class Test/game_data.csv', 'r') as file:
+    with open('Class Test/game_platform_data.csv', 'r') as file:
         reader = csv.reader(file)
 
         #Defining parallel arrays
         gameTitles = []
         genres = []
         ageRatings = []
+        platform = []
         #Setting genre_to_check to "Fantasy"
         genre_to_check = "Fantasy"
 
@@ -17,17 +25,18 @@ def readGameDataFromCSV():
             gameTitles.append(row[0])
             genres.append(row[1])
             ageRatings.append(row[2])
+            platform.append(row[3])
 
     #Returning the arrays
     return gameTitles, genres, ageRatings
 
 #Counting the suitable games in the genre "Fantasy" with an age rating of less than 18
-def countSuitableGames(genre_to_check, gameTitles, genres, ageRatings):
+def countSuitableGames(genre_to_check, gameTitles,genres,ageRatings):
     #Setting count to 0
     count = 0
     #Setting genre_to_check to "Fantasy"
     genre_to_check = "Fantasy"
-
+    
     #Loop to loop through each index in gameTitles
     for games in gameTitles:
         #Loop which checks if the current genre is "Fantasy" and if the age rating is less than 18
@@ -36,6 +45,19 @@ def countSuitableGames(genre_to_check, gameTitles, genres, ageRatings):
             #If the condition is true, 1 is added to count
             count = count + 1
     print(count)
+
+#Checks if the game is available on a given platform
+def availableOnPlatform():
+    #Opens a file to write to
+    with open('platform_suitable_games.txt', 'w') as file:
+
+        #Checks the conditions
+        if genre_to_check == "Fantasy" and ageRatings < 18 and platform == "PC":
+            #If the conditions are met, information is written to the file
+            file.write(gameTitles + "-" + platform)
+        else:
+            #If the conditions are not met, nothing is written to the file
+            file.write("")
 
 #Calling the subroutines
 def main():
