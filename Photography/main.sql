df4ceebb011385bcae718a7507473bac217b16ef
FAYALW <ALW001@stge.org.uk>
2024-11-14 14:21:42 +0000

    .

--- Photography/main.sql
+++ Photography/main.sql
@@ -1,1 +1,21 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+-- SELECT title, date_taken
+-- FROM Photos
+-- WHERE date_taken = (
+--     SELECT MAX(date_taken)
+--     FROM Photos
+-- );
+
+-- SELECT photographer_id, name
+-- FROM Photographers
+-- WHERE photographer_id NOT IN (
+--     SELECT photographer_id
+--     FROM Bookings
+-- );
+
+-- SELECT name
+-- FROM Clients
+-- WHERE client_id IN 
+--     (SELECT client_id 
+--     FROM Bookings 
+--     WHERE event_type = 'Wedding');
