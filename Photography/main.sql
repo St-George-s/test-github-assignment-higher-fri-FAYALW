a97e74f427623fe7fb3810c09b5a261b9b143c53
FAYALW <ALW001@stge.org.uk>
2024-11-15 09:54:30 +0000

    .

--- Photography/main.sql
+++ Photography/main.sql
@@ -1,21 +1,32 @@
 -- DESCRIBE ALL TABLES;
--- SELECT title, date_taken
--- FROM Photos
--- WHERE date_taken = (
---     SELECT MAX(date_taken)
---     FROM Photos
--- );
 
--- SELECT photographer_id, name
--- FROM Photographers
--- WHERE photographer_id NOT IN (
---     SELECT photographer_id
---     FROM Bookings
--- );
+Subqueries
 
--- SELECT name
--- FROM Clients
--- WHERE client_id IN 
---     (SELECT client_id 
---     FROM Bookings 
---     WHERE event_type = 'Wedding');
+SELECT title, date_taken
+FROM Photos
+WHERE date_taken = 
+    (SELECT MAX(date_taken)
+    FROM Photos);
+
+
+SELECT photographer_id, name
+FROM Photographers
+WHERE photographer_id NOT IN 
+    (SELECT photographer_id
+    FROM Bookings);
+
+
+SELECT name
+FROM Clients
+WHERE client_id IN 
+    (SELECT client_id 
+    FROM Bookings 
+    WHERE event_type = 'Wedding');
+
+
+SELECT COUNT(*) AS [Photo count]
+FROM Photos
+WHERE photographer_id IN 
+    (SELECT photographer_id 
+    FROM Photographers 
+    WHERE experience_years > 5);
