9bd2359a33929f5bb2e74324acdc24d48563bc01
FAYALW <ALW001@stge.org.uk>
2024-11-21 14:19:16 +0000

    .

--- Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
+++ Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
@@ -1,1 +1,13 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+-- SELECT gnomeName, SUM(quantity) AS "Total gnomes sold"
+-- FROM Gnome G, GnomePurchase GP
+-- WHERE 
+    -- G.description like "%solar%"
+    -- AND G.gnomeID = GP.gnomeID
+-- GROUP BY gnomeName
+-- ORDER BY "Total gnomes sold" DESC;
+
+
+SELECT emailAddress, CO.orderID, quantity, MAX(unitPrice)
+FROM Customer C, CustOrder CO, GnomePurchase GP, Gnome G
+WHERE quantity >= 3 AND 
