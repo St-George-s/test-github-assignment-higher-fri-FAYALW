1860f265fb6c2d555d024ec8ba3dfa4cfe90e6d9
FAYALW <ALW001@stge.org.uk>
2024-12-03 10:23:01 +0000

    Part B done

--- Database - Mock Test/main.sql
+++ Database - Mock Test/main.sql
@@ -1,1 +1,21 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+
+-- 3.1)
+-- SELECT eventName AS 'Event Name', name AS 'Shop Name', maxAttendees AS 'Max Attendees', eventDate AS 'Event Date'
+-- FROM Event, Shop
+-- WHERE Event.shopID = Shop.shopID;
+
+-- 3.2)
+-- SELECT name AS 'Shop Name', eventName AS 'Event Name'
+-- FROM Shop, Event
+-- WHERE Event.shopID = Shop.shopID AND eventDate = '2024-12-25';
+
+-- 3.3)
+-- UPDATE OpeningTime 
+-- SET closeTime = '19:00'
+-- WHERE date = '2024-12-24'
+-- AND shopID = (
+--    SELECT shopID
+--    FROM Shop
+--    WHERE name = 'Zara'
+-- );
