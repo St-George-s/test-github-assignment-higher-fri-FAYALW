598bd3f35781f2e3bd4f36cc8926822ce3238d8c
FAYALW <ALW001@stge.org.uk>
2025-01-30 14:23:14 +0000

    .

---
+++ Courseworks Databases/2022 Coursework Routes/Coding Answers.py
@@ -0,0 +1,26 @@
+#1b)
+def upperCase(userInput):
+    if userInput[0] != userInput[0].upper:
+        userInput[0] = userInput[0].upper
+    return userInput
+
+#1c)
+def readData(inputs):
+    with open(sightings.txt, 'r') as file:
+        content = file.read()
+    return inputs
+
+def age(inputs):
+    oldestWalker = inputs[0]
+    for input in inputs:
+        if 
+
+def dates():
+
+def sightings():
+
+
+def upperCase(userInput):
+    if userInput[0] != userInput[0].upper:
+        userInput[0] = userInput[0].upper
+    return userInput
