b38ce83cddf72bca8fce9ca341fef4c13e96900c
FAYALW <ALW001@stge.org.uk>
2025-01-31 09:46:43 +0000

    .

--- Courseworks Databases/2022 Coursework Routes/Coding Answers.py
+++ Courseworks Databases/2022 Coursework Routes/Coding Answers.py
@@ -1,8 +1,9 @@
 #1b)
-def upperCase(userInput):
-    if userInput[0] != userInput[0].upper:
-        userInput[0] = userInput[0].upper
-    return userInput
+def upperCase(userInputTown, userInputMammal):
+    if userInputTown[0] != userInputTown[0].upper or userInputMammal[0] != userInputMammal[0].upper:
+        userInputTown[0] = userInputTown[0].upper
+        userInputMammal[0] = userInputMammal[0].upper
+    return userInputTown, userInputMammal
 
 #1c)
 def readData(inputs):
@@ -19,7 +20,17 @@ def dates():
 
 def sightings():
 
-def upperCase(userInput):
-    if userInput[0] != userInput[0].upper:
-        userInput[0] = userInput[0].upper
-    return userInput
+def upperCase(userInputTown, userInputMammal):
+    if userInputTown[0] != userInputTown[0].upper or userInputMammal[0] != userInputMammal[0].upper:
+        userInputTown[0] = userInputTown[0].upper
+        userInputMammal[0] = userInputMammal[0].upper
+    return userInputTown, userInputMammal
+
+userInputTown = input('Enter the name of a town')
+upperCase(userInputTown, userInputMammal)
+userInputMammal = input('Enter the name of a mammal')
+upperCase(userInputTown, userInputMammal)
+
+for sighting in sightings:
+    if userInputTown = sightings.town and userInputMammal = sightings.mammal:
+    print(sighting.date)
