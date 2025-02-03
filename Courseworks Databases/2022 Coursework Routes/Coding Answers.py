46c9644846cba7e128e1be17baf7fb9d53049aac
FAYALW <ALW001@stge.org.uk>
2025-02-03 12:04:10 +0000

    Coursework

--- Courseworks Databases/2022 Coursework Routes/Coding Answers.py
+++ Courseworks Databases/2022 Coursework Routes/Coding Answers.py
@@ -1,70 +1,87 @@
-#1b)
-def upperCase(userInputTown, userInputMammal):
-    if userInputTown[0] != userInputTown[0].upper or userInputMammal[0] != userInputMammal[0].upper:
-        userInputTown[0] = userInputTown[0].upper
-        userInputMammal[0] = userInputMammal[0].upper
-    return userInputTown, userInputMammal
-
-#1c)
 import csv
 
-class sighting:
+#Define a record called Sighting
+class Sighting:
     def __init__(self, town, mammal, date, age):
-    self.town = towm
-    self.mammal = mammal
-    self.date = date
-    self.age = age
+        self.town = town
+        self.mammal = mammal
+        self.date = date
+        self.age = age
 
+#Reads data from the file into an array (sightings) of records (Sighting)
 def readData():
-    with open('Courseworks Databases/2022 Coursework Routes/setup/mammals.txt', 'r'')
+    with open('Courseworks Databases/2022 Coursework Routes/setup/mammals.txt', 'r') as file:
         reader = csv.reader(file)
-        header = next(reader)
+        sightings = []
 
         for row in reader:
-            newSighting = sighting(
+            newSighting = Sighting(
                 row[0],           
                 row[1],            
                 row[2],            
                 int(row[3]),                   
             )
             sightings.append(newSighting)
-    return sighting
+    return sightings
 
-def age(inputs):
-    oldestWalker = inputs[0]
-    for input in inputs:
-        if 
+def findMaxAge(sightings):
+    maximumValue = sightings[0]
+    for sighting in sightings:
+        if sighting.age > maximumValue.age:
+            maximumValue = sighting
+    print('The oldest walker is', maximumValue.age)  
 
-def date():
+def date(sightings):
+# 3.1 Ask user to enter town
+    town = input('Please enter a town')
+# 3.2 Call a function to return a string input that starts with an upper-case character
+    upperCase(town)
+# 3.3 Ask user to enter mammal
+    mammal = ('Please enter a mammal')
+# 3.4 Call a function to return a string input that starts with an upper-case character
+    upperCase(mammal)
+# 3.5 Display “The dates of sightings were:”
+    print('The dates of the sightings were:')
+# 3.6 Start loop for each sighting in array of records
+    for sighting in sightings:
+# 3.7 If sighting matches entered town and mammal then
+        if town == sighting.town and mammal == sighting.mammal:
+# 3.8 Display date
+            print(sighting.date)
+# 3.9 End if
+# 3.10 End loop
+
+#main program
+sightings = readData()
+print(sightings[0].town)
+
+oldestWalker = findMaxAge(sightings)
+print(oldestWalker)
+
+date(sightings)
+
+# def date():
 
 
-def sightings():
+# def sightings():
 
 
 
-def upperCase(userInputTown, userInputMammal):
-    firstCharTown = ord(userInputTown[0]) 
-    firstCharMammal = ord(userInputMammal[0])
-    if firstCharTown >= 97 and firstCharTown =< 122 or firstCharMammal >= 97 and firstCharMammal =< 122:
-        firstCharTown = firstCharTown - 32
-        firstCharMammal = firstCharMammal -32
-        town = (firstCharTown + userInputTown[1:])
-        mammal = (firstCharMammal + userInputMammal[1:])
-    
+# def upperCase(userInputTown, userInputMammal):
+#     firstCharTown = ord(userInputTown[0]) 
+#     firstCharMammal = ord(userInputMammal[0])
+#     if firstCharTown >= 97 and firstCharTown =< 122 or firstCharMammal >= 97 and firstCharMammal =< 122:
+#         firstCharTown = firstCharTown - 32
+#         firstCharMammal = firstCharMammal -32
+#         town = (firstCharTown + userInputTown[1:])
+#         mammal = (firstCharMammal + userInputMammal[1:])
 
+# userInputTown = input('Enter the name of a town')
+# upperCase(userInputTown, userInputMammal)
+# userInputMammal = input('Enter the name of a mammal')
+# upperCase(userInputTown, userInputMammal)
 
-
-
-
-
-
-
-userInputTown = input('Enter the name of a town')
-upperCase(userInputTown, userInputMammal)
-userInputMammal = input('Enter the name of a mammal')
-upperCase(userInputTown, userInputMammal)
-
-for sighting in sightings:
-    if userInputTown == sightings.town and userInputMammal == sightings.mammal
-    print(sighting.date)
+# for sighting in sightings:
+#     if userInputTown == sightings.town and userInputMammal == sightings.mammal
+#     print(sighting.date)
 
