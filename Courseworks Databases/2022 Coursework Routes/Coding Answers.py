17c61c23fc72217fca73687fb2ecca55518fd269
FAYALW <ALW001@stge.org.uk>
2025-02-04 10:13:40 +0000

    Coding for part A done

--- Courseworks Databases/2022 Coursework Routes/Coding Answers.py
+++ Courseworks Databases/2022 Coursework Routes/Coding Answers.py
@@ -1,45 +1,47 @@
 import csv
 
 #Define a record called Sighting
-class Sighting:
-    def __init__(self, town, mammal, date, age):
-        self.town = town
-        self.mammal = mammal
-        self.date = date
-        self.age = age
+class Sighting: #Creates a record
+    def __init__(self, town, mammal, date, age): #Initialises the cetegories in the record
+        self.town = town #Defining each category in the record
+        self.mammal = mammal #Defining each category in the record
+        self.date = date #Defining each category in the record
+        self.age = age #Defining each category in the record
 
 #Reads data from the file into an array (sightings) of records (Sighting)
-def readData():
-    with open('Courseworks Databases/2022 Coursework Routes/setup/mammals.txt', 'r') as file:
-        reader = csv.reader(file)
-        sightings = []
-
-        for row in reader:
-            newSighting = Sighting(
-                row[0],           
-                row[1],            
-                row[2],            
-                int(row[3]),                   
+def readData(): #Defines function
+    with open('Courseworks Databases/2022 Coursework Routes/setup/mammals.txt', 'r') as file: #Opens a file
+        reader = csv.reader(file) #Reads into the file
+        sightings = [] #Creates an array called sightings
+        for row in reader: #Loops through each row in the file
+            newSighting = Sighting( #For each row in the file it makes a new record for each sighting
+                row[0], #Puts town in the first row     
+                row[1], #Mammal in the second row           
+                row[2], #Date in the third row           
+                int(row[3]), #Age in the fourth row                   
             )
-            sightings.append(newSighting)
-    return sightings
+            sightings.append(newSighting) #Appends each new record in the array
+    return sightings #Returning the array of records to the main program
 
-def findMaxAge(sightings):
-    maximumValue = sightings[0]
+#Finds the oldest walker
+def findMaxAge(sightings): #Defines function 
+    maximumValue = sightings[0] #
     for sighting in sightings:
         if sighting.age > maximumValue.age:
             maximumValue = sighting
     print('The oldest walker is', maximumValue.age)  
 
+#Finds the dates of sightings
 def date(sightings):
-    townInput = input('Please enter a town')
-    upperCase(townInput)
-    mammalInput = ('Please enter a mammal')
-    upperCase(mammalInput)
+    townInput = input('Please enter a town: ')
+    townInput = upperCase(townInput)
+    mammalInput = input('Please enter a mammal: ')
+    mammalInput = upperCase(mammalInput)
     for sighting in sightings:
         if townInput == sighting.town and mammalInput == sighting.mammal:
             print('The dates of the sightings were:', sighting.date)
 
+#Changed the first character to uppercase
 def upperCase(word):
     firstChar = ord(word[0])
     if firstChar >=97 and firstChar <= 122:
@@ -48,29 +50,21 @@ def upperCase(word):
         word = (firstChar + word[1:])
     return word
 
+#Calculates the number of sightings for each date
 def numberOfSightings(sightings):
-# 4.1 Set dayToCount to first date in sightings array
-    dayToCount = sightings.date[0]
-# 4.2 Set count to 1
+    dayToCount = sightings[0].date
     count = 1
-# 4.3 Start loop from second record to end of sightings array
-    for x in range (1, len(sightings)):
-# 4.4 If date in current record is the same as dayToCount then
-        if x == dayToCount:
-# 4.5 Add 1 to count
+    for counter in range (1, len(sightings)):
+        if sightings[counter].date == dayToCount:
             count = count + 1
-# 4.6 Else
         else:
-# 4.7 Display dayToCount and count
             print(dayToCount, count)
-# 4.8 Set dayToCount to date in current record
-            dayToCount = x
-# 4.9 Set count to 1
+            dayToCount = sightings[counter].date
             count = 1
 
 #main program
 sightings = readData()
-print(sightings[0].town)
+# print(sightings[0].town)
 
 oldestWalker = findMaxAge(sightings)
 print(oldestWalker)
@@ -78,3 +72,8 @@ print(oldestWalker)
 date(sightings)
 
 numberOfSightings(sightings)
+
+#sightings[counter].date
+#sightings = array
+#counter = position in array
+#date = category in the record
