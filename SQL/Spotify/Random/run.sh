cc600c50466141acac30bdcaa26bfc6891aaa2f3
FAYALW <ALW001@stge.org.uk>
2025-02-07 18:55:48 +0000

    Organizing

--- SQL/Spotify/Random/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-poetry run python setup/main.py
--- SQL/Spotify/Setup/albums.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Albums (
-    album_id INT PRIMARY KEY,
-    album_name VARCHAR(255) NOT NULL,
-    release_year INT
-);
-
-INSERT INTO Albums (album_id, album_name, release_year) VALUES (1, 'Parachutes', 2000), (2, '25', 2015), (3, 'Divide', 2017);
-
