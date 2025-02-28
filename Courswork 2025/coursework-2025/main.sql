a836697d379e240d04fef1d26d138f7efacb3611
FAYALW <ALW001@stge.org.uk>
2025-02-28 10:06:40 +0000

    .

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,1 +1,30 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+
+-- 2c
+-- SELECT comicTitle, issue, publisherName, valuation
+-- FROM Comic, Publisher
+-- WHERE valuation > (
+--     SELECT AVG(valuation)
+--     FROM Comic
+
+-- )
+
+-- 2d
+-- SELECT characterName, SUM(valuation) AS 'Total Valuation'
+-- FROM CharacterInfo, Comic, ComicCharacter
+-- WHERE CharacterInfo.characterID = comicCharacter.characterID
+-- AND ComicCharacter.comicID = Comic.comic
+-- AND characterName like '%Duck%'
+-- GROUP BY characterName
+-- ORDER BY 'Total Valuation' ASC;
+
+-- 2e
+SELECT comicTitle, issue, publisherName, (valuation * 2) AS [Double Price]
+FROM Comic, Publisher, Series, CharacterInfo, ComicCharacter
+WHERE Series.seriesName = "The OK Seven"
+AND CharacterInfo.characterName = "Starlordly"
+AND Comic.publisherID = Publisher.publisherID
+AND Comic.seriesID = Series.seriesID
+AND CharacterInfo.characterID = ComicCharacter.characterID
+GROUP BY comicTitle
+ORDER BY [Double Price] DESC;
