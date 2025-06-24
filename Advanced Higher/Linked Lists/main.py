e87fb827f481837b0c49ae1183eb38bc4ed23974
FAYALW <ALW001@stge.org.uk>
2025-06-24 09:18:55 +0000

    .

--- Advanced Higher/Linked Lists/main.py
+++ Advanced Higher/Linked Lists/main.py
@@ -4,4 +4,8 @@ from taskList import TaskList
 tasks = TaskList()
 tasks.add_task("Maths", "Finish worksheet", "2025-06-20")
 tasks.add_task("English", "Read Chapter 5", "2025-06-19")
+tasks.add_task("History", "Complete textbook questions", "2025-06-18")
+tasks.complete_task()
+tasks.complete_task()
+tasks.complete_task()
 tasks.print_tasks()
