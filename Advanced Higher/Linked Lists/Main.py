47e60c626d2924e7ae4136b420ce9e0abd5ae1f4
FAYALW <ALW001@stge.org.uk>
2025-06-24 09:11:00 +0000

    .

---
+++ Advanced Higher/Linked Lists/Main.py
@@ -0,0 +1,7 @@
+from taskNode import TaskNode
+from taskList import TaskList
+
+tasks = TaskList()
+tasks.add_task("Maths", "Finish worksheet", "2025-06-20")
+tasks.add_task("English", "Read Chapter 5", "2025-06-19")
+tasks.print_tasks()
