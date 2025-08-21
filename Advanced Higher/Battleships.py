73269cfe7646e312a3b44657460d5cae05c0fe9d
FAYALW <ALW001@stge.org.uk>
2025-08-21 09:14:06 +0000

    .

--- Advanced Higher/Battleships.py
+++ Advanced Higher/Battleships.py
@@ -1,13 +1,22 @@
-grid = [[".", ".", "s", "s", "s"], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."]]
+grid = [[".", ".", "s", "s", "s"], 
+        [".", ".", ".", ".", "."], 
+        [".", ".", ".", ".", "."], 
+        [".", ".", ".", ".", "."], 
+        [".", ".", ".", ".", "."]]
+
+# print(grid[0][2])
 
 hits = 0
 while hits < 3:
-    userGuess = int(input("Please guess a number from 1-5: "))
-    if grid[userGuess] == "s":
-        print("You hit the ship!")
+    userGuessRow = int(input("Please guess a row from 1-5: "))
+    userGuessColumn = int(input("Please guess a column from 1-5: "))
+    if grid[userGuessRow][userGuessColumn] == "s":
+        print("You hit the ship")
         hits += 1
 
-    elif grid[userGuess] == ".":
+    elif grid[userGuessRow][userGuessColumn] == ".":
         print("You missed the ship!")
-        grid[userGuess] = "m"
+        grid[userGuessRow][userGuessColumn] = "m"
+
+
 
