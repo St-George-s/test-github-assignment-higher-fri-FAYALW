c1a213283a803aaeba87e001e955156420f6b85d
FAYALW <ALW001@stge.org.uk>
2025-08-25 14:05:57 +0000

    .

--- Advanced Higher/Battleships.py
+++ Advanced Higher/Battleships.py
@@ -1,3 +1,4 @@
+#Making a grid with a ship in a location
 grid = [[".", ".", "s", "s", "s"], 
         [".", ".", ".", ".", "."], 
         [".", ".", ".", ".", "."], 
@@ -6,8 +7,9 @@ grid = [[".", ".", "s", "s", "s"],
 
 # print(grid[0][2])
 
-hits = 0
-while hits < 3:
+#If they have hit less than 3 times then this loop runs
+hits = 0 #Initialises a counter called hits and sets it to 0
+while hits < 3: #While  
     userGuessRow = int(input("Please guess a row from 1-5: "))-1
     userGuessColumn = int(input("Please guess a column from 1-5: "))-1
     if grid[userGuessRow][userGuessColumn] == "s":
---
+++ Advanced Higher/Linked Lists/Post Summer OO Recap.py
@@ -0,0 +1,20 @@
+class Ticket:
+    def __init__(self, user, issue):
+        self.user = user
+        self.issue = issue
+        self.next = None
+    
+class HelpDesk:
+    def __init__(self):
+        self.head = None
+
+    def log_ticket(self, user, issue):
+        new_ticket = Ticket(user, issue)
+        new_ticket.next = self.head
+        self.head = new_ticket
+
+    def show_tickets(self):
+        current = self.head
+        while current is not None:
+            print(f"{current.user} reported: {current.issue}")
+            current = current.next
