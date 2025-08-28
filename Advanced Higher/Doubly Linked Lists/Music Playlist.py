16a1364b1838c52c5d2a05a0f1058f02366178b0
FAYALW <ALW001@stge.org.uk>
2025-08-28 09:06:27 +0000

    Doubly linked lists code

--- Advanced Higher/Doubly Linked Lists/Music Playlist.py
+++ Advanced Higher/Doubly Linked Lists/Music Playlist.py
@@ -1,3 +1,5 @@
+#A node stores data, and links to the next and previous nodes
+
 class SongNode:
     def __init__(self, title):
         self.title = title
@@ -6,8 +8,8 @@ class SongNode:
 
 class Playlist:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = None #First song in the playlist
+        self.tail = None #Last song in the playlist
 
     def add_song(self, title):
         new_song = SongNode(title)
@@ -22,4 +24,18 @@ class Playlist:
     def play_forward(self):
         current = self.head
         while current:
-            print("Now playing:")
+            print("Now playing:", current.title)
+            current = current.next
+
+#Implement myself
+    def play_backward(self):
+        pass
+
+#Searching forward through the playlist
+    def find_song_forward(self, title):
+        current = self.head
+        while current:
+            if current.title == title:
+                return True
+            current = current.next
+        return False
