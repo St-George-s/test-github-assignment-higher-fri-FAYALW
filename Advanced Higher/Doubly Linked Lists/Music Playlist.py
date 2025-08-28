570116d87ea81f54edd8eb166f223acc46ba309a
FAYALW <ALW001@stge.org.uk>
2025-08-28 08:12:30 +0000

    Working on double linked list

---
+++ Advanced Higher/Doubly Linked Lists/Music Playlist.py
@@ -0,0 +1,18 @@
+class SongNode:
+    def __init__(self, title):
+        self.title = title
+        self.next = None
+        self.prev = None
+
+class Playlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_song(self, title):
+        new_song = SongNode(title)
+        if self.head is None:
+            self.head = new_song
+            self.tail = new_song
+        else:
+            self.tail.next = 
