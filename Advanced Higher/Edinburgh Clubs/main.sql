169777ed56025fc2d98670626300192d9f14287b
FAYALW <ALW001@stge.org.uk>
2025-09-23 12:44:30 +0000

    Database working!!

--- Advanced Higher/Edinburgh Clubs/main.sql
+++ Advanced Higher/Edinburgh Clubs/main.sql
@@ -1,33 +1,34 @@
 -- CREATE DATABASE SwimClubDB;
 USE SwimClubDB;
 
-CREATE TABLE Centre(
-    centreID INT NOT NULL PRIMARY KEY,
-    centreName VARCHAR(40) NOT NULL,
-    centreType VARCHAR(20) NOT NULL
-)
+CREATE TABLE Centre ( 
+  centreID INT NOT NULL PRIMARY KEY, 
+  centreName VARCHAR(40) NOT NULL, 
+  centreType VARCHAR(20) NOT NULL 
+); 
 
 
-CREATE TABLE Class(
-    classCode VARCHAR(4) NOT NULL PRIMARY KEY,
-    className VARCHAR(40) NOT NULL,
-    centreID INT NOT NULL,
-    level INT NOT NULL,
-    termStartDate DATE,
-    sessionType VARCHAR(12) NOT NULL,
-    startTime TIME NOT NULL,
-    pricePerPerson DECIMAL(6,2) NOT NULL,
-    FOREIGN KEY (centreID) REFERENCES Centre(centreID)
-)
+CREATE TABLE Class ( 
+  classCode VARCHAR(4) NOT NULL PRIMARY KEY, 
+  className VARCHAR(40) NOT NULL, 
+  centreID INT NOT NULL, 
+  level INT NOT NULL, 
+  termStartDate DATE, 
+  sessionType VARCHAR(12) NOT NULL,     -- e.g. Term / Drop-in / Intensive 
+  startTime TIME NOT NULL, 
+  pricePerPerson DECIMAL(6,2) NOT NULL, 
+  FOREIGN KEY (centreID) REFERENCES Centre(centreID) 
+); 
 
-CREATE TABLE Member(
-    memberNo INT NOT NULL PRIMARY KEY,
-    firstName VARCHAR(20) NOT NULL,
-    surname VARCHAR(20) NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    town VARCHAR(30) NOT NULL,
-    postcode VARCHAR(10) NOT NULL
-)
+
+CREATE TABLE Member ( 
+  memberNo INT NOT NULL PRIMARY KEY,
+  firstName VARCHAR(20) NOT NULL, 
+  surname VARCHAR(20) NOT NULL, 
+  address VARCHAR(60) NOT NULL, 
+  town VARCHAR(30) NOT NULL, 
+  postcode VARCHAR(10) NOT NULL 
+); 
 
 
 CREATE TABLE Booking ( 
