b46928b189198438877052c40330c21ebbdc0589
FAYALW <ALW001@stge.org.uk>
2026-01-29 10:25:43 +0000

    ,

--- Advanced Higher/Example DB/main.sql
+++ Advanced Higher/Example DB/main.sql
@@ -1,20 +1,19 @@
 CREATE DATABASE IF NOT EXISTS project1;
 USE project1;
 
+DROP TABLE IF EXISTS Tasks;
 
 CREATE TABLE IF NOT EXISTS Tasks(
   taskID INT PRIMARY KEY AUTO_INCREMENT,
-  taskName VARCHAR(100) NOT NULL
-  categoryID INT
-  category VARCHAR(25)
-  dueDate DATE 
+  taskName VARCHAR(100) NOT NULL,
+  categoryID INT,
+  category VARCHAR(25),
+  dueDate DATE,
   completionStatus BOOLEAN
 );
 
 
-INSERT INTO Tasks(taskName) VALUES
-('Buy groceries'),
-('Clean house');
 
-SELECT *
-FROM Tasks;
+INSERT INTO Tasks(taskID, taskName, categoryID, category, dueDate, completionStatus) VALUES
+(1, "Buy groceries", 3, "Household", "2026-12-12", TRUE);
+
